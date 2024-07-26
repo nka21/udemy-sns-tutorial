3c90cf742da84a1c102636e8eb000dba7c52141a
nka21 <naoki.nico@gmail.com>
2024-07-27 00:47:17 +0900

    chore: ユーザーログインAPI

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // ローカルサーバーの構築
 const app = express();
@@ -35,6 +37,34 @@ app.post("/api/auth/register", async (req, res) => {
   });
 
   return res.json({ user });
+});
+
+// ユーザーログインAPI
+app.post("/api/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  // emailを検索
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) {
+    return res
+      .status(401)
+      .json({ error: "そのようなユーザーは存在しません。" });
+  }
+
+  // パスワードの確認
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ error: "そのパスワードは間違っています。" });
+  }
+
+  // JWTトークンの生成
+  const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+    expiresIn: "1d",
+  });
+
+  return res.json({ token });
 });
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
