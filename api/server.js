147342b8de478da4a0c5ebfee63a99e9a757bba9
nka21 <naoki.nico@gmail.com>
2024-07-26 13:56:35 +0900

    chore: 3つのデータをJSONでレスポンス
    PrismaClientの使い方は、以下ドキュメントを参照
    https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases/querying-the-database-typescript-postgresql

--- api/server.js
+++ api/server.js
@@ -1,8 +1,31 @@
 const express = require("express");
+import { PrismaClient } from "@prisma/client";
 
 // ローカルサーバーの構築
 const app = express();
 
 const PORT = 8000;
+
+// インスタンス化
+const prisma = new PrismaClient();
+
+// 新規ユーザー登録API
+app.post("/api/auth/register", async (req, res) => {
+  // フォームから送信されるデータ
+  const { username, email, password } = req.body;
+
+  /*
+    https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases/querying-the-database-typescript-postgresql
+  */
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password,
+    },
+  });
+
+  return res.json({ user });
+});
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
