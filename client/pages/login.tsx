b13805c5af3c7c25af98db3ff51102e1a5646051
nka21 <naoki.nico@gmail.com>
2024-07-28 23:56:02 +0900

    chore: login時に、localStorageへauth_tokenを保存するのを実装
    auth.tsxで、定義したuseAuthフックを使って、
    引数に、tokenを代入

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -2,12 +2,15 @@ import Head from "next/head";
 import { useState } from "react";
 import axiosInstance from "@/lib/apiClient";
 import { useRouter } from "next/router";
+import { useAuth } from "./auth";
 
 const Login = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
   const router = useRouter();
+
+  const { login } = useAuth();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -20,7 +23,8 @@ const Login = () => {
       });
 
       const token = response.data.token;
-      console.log(token);
+
+      login(token);
 
       router.push("/");
     } catch (error) {
