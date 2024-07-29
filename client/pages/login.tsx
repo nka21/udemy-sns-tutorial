494cc1f93403c249c7b8a5ca6dbe65d908e67ccf
nka21 <naoki.nico@gmail.com>
2024-07-29 15:16:20 +0900

    chore: 型の指定

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -81,7 +81,7 @@ const Login = () => {
                 autoComplete="current-password"
                 required
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 text-base focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
                   setPassword(e.target.value)
                 }
               />
