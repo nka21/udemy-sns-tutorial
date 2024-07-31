cf59baf3295f1494a1aef64238f362942db78f95
nka21 <naoki.nico@gmail.com>
2024-07-31 20:56:13 +0900

    fix: `auth.tsx` を `context/auth.tsx` に移動したため、import文を修正

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 
 import Navbar from "@/components/Navbar";
-import { AuthProvider } from "./auth";
+import { AuthProvider } from "@/context/auth";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
