4d9ec72a988386e0ef74ad64cb883808b7e8ac4d
nka21 <naoki.nico@gmail.com>
2024-08-02 14:08:55 +0900

    chore: ルーティングの追加設定

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const authRoute = require("./routers/auth");
 const postsRoute = require("./routers/posts");
+const usersRoute = require("./routers/users");
 
 // 環境変数の設定
 dotenv.config();
@@ -23,6 +24,7 @@ app.use(cors());
 // ルートの設定
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postsRoute);
+app.use("/api/users", usersRoute);
 
 // サーバーの起動
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
